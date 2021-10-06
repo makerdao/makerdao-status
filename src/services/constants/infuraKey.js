af6b22bf37b60d64dab4307390d74c847e667c50
Diego Escobar <diego.netsolutions@gmail.com>
2021-10-06 15:00:51 +0300

    updated mainnet infura provider apikey

--- src/services/constants/infuraKey.js
+++ src/services/constants/infuraKey.js
@@ -1,1 +1,3 @@
-export const infuraKey = "2c010c2fdb8b4ef1a7617571553fc982";
+export const infuraKey = "30b97714b3d04bedad988cd6c00bfea5";
+
+// export const infuraKey = "2c010c2fdb8b4ef1a7617571553fc982";
