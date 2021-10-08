694de2016116ccbe6b2c23bd8f8ee475c0c0fd92
Yanier Alfonso <yanier.alfonso@dspot.com.pl>
2021-10-08 15:45:30 -0200

    adding svg icons

---
+++ src/components/icon/svg/OverviewIcon.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+interface Props {
+  width?: number;
+  height?: number;
+  fill?: string | number;
+}
+
+function OverviewIcon({
+  width = 50,
+  height = 50,
+  fill = "white",
+  ...props
+}: Props & React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg width={width} height={height} viewBox="0 0 56 38" fill={fill} {...props}>
+      <path
+        d="M22.75 12.467c0-.902.731-1.633 1.633-1.633h12.484a1.633 1.633 0 010 3.266H24.383a1.633 1.633 0 01-1.633-1.633z"
+        fill="#F5F6FA"
+      />
+      <path
+        d="M22.75 12.467c0-.902.731-1.633 1.633-1.633h12.484a1.633 1.633 0 010 3.266H24.383a1.633 1.633 0 01-1.633-1.633z"
+        stroke="#F5F6FA"
+      />
+      <path
+        d="M22.75 19c0-.902.731-1.633 1.633-1.633h12.484a1.633 1.633 0 010 3.266H24.383A1.633 1.633 0 0122.75 19z"
+        fill="#F5F6FA"
+      />
+      <path
+        d="M22.75 19c0-.902.731-1.633 1.633-1.633h12.484a1.633 1.633 0 010 3.266H24.383A1.633 1.633 0 0122.75 19z"
+        stroke="#F5F6FA"
+      />
+      <path
+        d="M22.75 25.533c0-.902.731-1.633 1.633-1.633h12.484a1.633 1.633 0 010 3.267H24.383a1.633 1.633 0 01-1.633-1.634z"
+        fill="#F5F6FA"
+      />
+      <path
+        d="M22.75 25.533c0-.902.731-1.633 1.633-1.633h12.484a1.633 1.633 0 010 3.267H24.383a1.633 1.633 0 01-1.633-1.634z"
+        stroke="#F5F6FA"
+      />
+      <path
+        d="M17.5 25.475a1.575 1.575 0 013.15 0v.117a1.575 1.575 0 01-3.15 0v-.117z"
+        fill="#F5F6FA"
+      />
+      <path
+        d="M17.5 25.475a1.575 1.575 0 013.15 0v.117a1.575 1.575 0 01-3.15 0v-.117z"
+        stroke="#F5F6FA"
+      />
+      <path
+        d="M17.5 18.942a1.575 1.575 0 013.15 0v.116a1.575 1.575 0 01-3.15 0v-.116z"
+        fill="#F5F6FA"
+      />
+      <path
+        d="M17.5 18.942a1.575 1.575 0 013.15 0v.116a1.575 1.575 0 01-3.15 0v-.116z"
+        stroke="#F5F6FA"
+      />
+      <path
+        d="M17.5 12.409a1.575 1.575 0 113.15 0v.116a1.575 1.575 0 01-3.15 0v-.117z"
+        fill="#F5F6FA"
+      />
+      <path
+        d="M17.5 12.409a1.575 1.575 0 113.15 0v.116a1.575 1.575 0 01-3.15 0v-.117z"
+        stroke="#F5F6FA"
+      />
+    </svg>
+  );
+}
+
+const MemoOverview = React.memo(OverviewIcon);
+export default MemoOverview;
---
+++ src/components/icon/svg/RightArrowIcon.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+interface Props {
+  width?: number;
+  height?: number;
+  fill?: string | number;
+}
+
+function RightArrowIcon({
+  width = 18,
+  height = 18,
+  fill = "white",
+  ...props
+}: Props & React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width={width}
+      height={height}
+      fill={fill}
+      viewBox="0 0 256 256"
+      {...props}
+    >
+      <g
+        transform="rotate(180 127.018 127.018) scale(2.8008)"
+        strokeWidth={0}
+        strokeMiterlimit={10}
+        fill="none"
+        style={{
+          stroke: "none",
+          strokeWidth: 0,
+          strokeDasharray: "none",
+          strokeLinecap: "butt",
+          strokeLinejoin: "miter",
+          strokeMiterlimit: 10,
+          fill: "none",
+          fillRule: "nonzero",
+          opacity: 1,
+        }}
+      >
+        <circle cx={45} cy={45} r={45} fill="#1aab9b" />
+        <path
+          d="M70.191 51.154H13.5a3.001 3.001 0 01-2.285-4.944l18.258-21.462a3 3 0 014.57 3.888L19.991 45.154h50.201a3 3 0 11-.001 6z"
+          fill="#1ba7ad"
+        />
+        <path
+          d="M31.759 72.617a2.992 2.992 0 01-2.287-1.057L11.215 50.098a2.998 2.998 0 012.285-4.943h56.691a3 3 0 110 6h-50.2l14.052 16.52a3 3 0 01-.341 4.229 2.995 2.995 0 01-1.943.713z"
+          fill="#1ba7ad"
+        />
+        <path
+          d="M73.5 47.846H16.809a3.001 3.001 0 01-2.285-4.944L32.781 21.44a3 3 0 114.57 3.888L23.299 41.846H73.5a3 3 0 110 6z"
+          fill="#fff"
+        />
+        <path
+          d="M35.067 69.309a2.99 2.99 0 01-2.287-1.057L14.523 46.789a2.998 2.998 0 012.285-4.943H73.5a3 3 0 110 6H23.299l14.052 16.52a3 3 0 01-2.284 4.943z"
+          fill="#fff"
+        />
+      </g>
+    </svg>
+  );
+}
+
+const MemoRightArrow = React.memo(RightArrowIcon);
+export default MemoRightArrow;
