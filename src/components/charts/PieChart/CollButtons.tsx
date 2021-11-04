d9397913cefb42ab3e083ec890126ebcb0a34884
Yanier Alfonso <yanier.alfonso@dspot.com.pl>
2021-11-04 11:28:16 -0200

    adding the selected state in the buttoms of the legend

---
+++ src/components/charts/PieChart/CollButtons.tsx
@@ -0,0 +1,132 @@
+import React from 'react';
+
+export type ButtonValues = 'collateral' | 'collateralAuction';
+interface Props {
+  onButtonSelect: (value: ButtonValues) => void;
+}
+
+function CollButtons({ onButtonSelect }: Props) {
+  const onClick = (value: ButtonValues) => () => {
+    onButtonSelect(value);
+  };
+  return (
+    <>
+      <path
+        onClick={onClick('collateralAuction')}
+        opacity={0.1}
+        d="M157 6h138.002c5.523 0 10 4.477 10 10v19c0 5.523-4.477 10-10 10H157V6z"
+        fill="#B8C5D3"
+        style={{
+          cursor: 'pointer',
+        }}
+      />
+      <text
+        onClick={onClick('collateralAuction')}
+        fill="#31394D"
+        fillOpacity={0.5}
+        style={{
+          whiteSpace: 'pre',
+          cursor: 'pointer',
+        }}
+        fontFamily="Roboto"
+        fontSize={12}
+        letterSpacing={0}
+      >
+        <tspan x={184.295} y={29.102}>
+          Collateral Auction
+        </tspan>
+      </text>
+      <g filter="url(#prefix__filter0_d_2307:36659)">
+        <g opacity={0.06} filter="url(#prefix__filter1_d_2307:36659)">
+          <path
+            onClick={onClick('collateral')}
+            d="M10 16c0-5.523 4.477-10 10-10h137v39H20c-5.523 0-10-4.477-10-10V16z"
+            fill="#fff"
+            style={{
+              cursor: 'pointer',
+            }}
+          />
+        </g>
+        <text
+          onClick={onClick('collateral')}
+          fill="#31394D"
+          style={{
+            whiteSpace: 'pre',
+            cursor: 'pointer',
+          }}
+          fontFamily="Roboto"
+          fontSize={15}
+          fontWeight={500}
+          letterSpacing={0}
+        >
+          <tspan x={51.406} y={30.127}>
+            Collateral
+          </tspan>
+        </text>
+      </g>
+      <defs>
+        <filter
+          id="prefix__filter0_d_2307:36659"
+          x={5}
+          y={2}
+          width={157}
+          height={49}
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity={0} result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy={1} />
+          <feGaussianBlur stdDeviation={2.5} />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix values="0 0 0 0 0.5125 0 0 0 0 0.5125 0 0 0 0 0.5125 0 0 0 0.3 0" />
+          <feBlend
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_2307:36659"
+          />
+          <feBlend
+            in="SourceGraphic"
+            in2="effect1_dropShadow_2307:36659"
+            result="shape"
+          />
+        </filter>
+        <filter
+          id="prefix__filter1_d_2307:36659"
+          x={0.97}
+          y={0.97}
+          width={165.06}
+          height={57.06}
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity={0} result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy={4} />
+          <feGaussianBlur stdDeviation={4.515} />
+          <feColorMatrix values="0 0 0 0 0.690196 0 0 0 0 0.745098 0 0 0 0 0.772549 0 0 0 0.25 0" />
+          <feBlend
+            mode="multiply"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_2307:36659"
+          />
+          <feBlend
+            in="SourceGraphic"
+            in2="effect1_dropShadow_2307:36659"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </>
+  );
+}
+
+const MemoCollButtons = React.memo(CollButtons);
+export default MemoCollButtons;
--- src/components/charts/PieChart/Legend.tsx
+++ src/components/charts/PieChart/Legend.tsx
@@ -1,4 +1,6 @@
-import React, { PropsWithChildren } from 'react';
+import React from 'react';
+import MemoCollButtons from './CollButtons';
+import CollAuctionButtons from './CollAuctionButtons';
 import MemoCollateralAuctionLegend from './CollateralAuctionLegend';
 import MemoCollateralLegend from './CollateralLegend';
 
@@ -26,46 +28,6 @@ function Legend({
   collateral,
   collateralAuction,
 }: Props) {
-  const onClick = (value: ButtonValues) => () => {
-    onButtonSelect(value);
-  };
-  const SelectedContainer = ({
-    children,
-    selected,
-  }: PropsWithChildren<{ selected: boolean }>) => {
-    if (selected) {
-      return (
-        <g filter="url(#prefix__filter0_d_3932:1308)">
-          <g opacity={0.06} filter="url(#prefix__filter1_d_3932:1308)">
-            <path
-              onClick={onClick('collateral')}
-              d="M0 10C0 4.477 4.477 0 10 0h137v39H10C4.477 39 0 34.523 0 29V10z"
-              fill="#fff"
-              style={{
-                cursor: 'pointer',
-              }}
-            />
-          </g>
-          {children}
-        </g>
-      );
-    }
-    return (
-      <g>
-        <path
-          onClick={onClick('collateralAuction')}
-          opacity={0.1}
-          d="M147 0h138.002c5.523 0 10 4.477 10 10v19c0 5.523-4.477 10-10 10H147V0z"
-          fill="#B8C5D3"
-          style={{
-            cursor: 'pointer',
-          }}
-        />
-        {children}
-      </g>
-    );
-  };
-
   return (
     <svg
       width="240px"
@@ -76,93 +38,17 @@ function Legend({
       fill="none"
     >
       {buttonSelected === 'collateral' && (
-        <MemoCollateralLegend {...collateral} />
+        <>
+          <MemoCollButtons onButtonSelect={onButtonSelect} />
+          <MemoCollateralLegend {...collateral} />
+        </>
       )}
       {buttonSelected === 'collateralAuction' && (
-        <MemoCollateralAuctionLegend {...collateralAuction} />
+        <>
+          <CollAuctionButtons onButtonSelect={onButtonSelect} />
+          <MemoCollateralAuctionLegend {...collateralAuction} />
+        </>
       )}
-      <SelectedContainer selected={false}>
-        <text
-          fill="#31394D"
-          fillOpacity={0.5}
-          onClick={onClick('collateralAuction')}
-          style={{
-            whiteSpace: 'pre',
-            cursor: 'pointer',
-          }}
-          fontFamily="Roboto"
-          fontSize={12}
-          letterSpacing={0}
-        >
-          <tspan x={174.295} y={23.102}>
-            Collateral Auction
-          </tspan>
-        </text>
-      </SelectedContainer>
-      <SelectedContainer selected>
-        <text
-          fill="#31394D"
-          onClick={onClick('collateral')}
-          style={{
-            whiteSpace: 'pre',
-            cursor: 'pointer',
-          }}
-          fontFamily="Roboto"
-          fontSize={15}
-          fontWeight={500}
-          letterSpacing={0}
-        >
-          <tspan x={41.406} y={24.127}>
-            Collateral
-          </tspan>
-        </text>
-      </SelectedContainer>
-      <defs>
-        <filter
-          id="prefix__filter0_d_3932:1308"
-          x={-5}
-          y={-4}
-          width={157}
-          height={49}
-          filterUnits="userSpaceOnUse"
-          colorInterpolationFilters="sRGB"
-        >
-          <feFlood floodOpacity={0} result="BackgroundImageFix" />
-          <feColorMatrix
-            in="SourceAlpha"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-            result="hardAlpha"
-          />
-          <feOffset dy={1} />
-          <feGaussianBlur stdDeviation={2.5} />
-          <feComposite in2="hardAlpha" operator="out" />
-          <feColorMatrix values="0 0 0 0 0.5125 0 0 0 0 0.5125 0 0 0 0 0.5125 0 0 0 0.3 0" />
-          <feBlend
-            in2="BackgroundImageFix"
-            result="effect1_dropShadow_3932:1308"
-          />
-          <feBlend
-            in="SourceGraphic"
-            in2="effect1_dropShadow_3932:1308"
-            result="shape"
-          />
-        </filter>
-        <filter
-          id="prefix__filter1_d_3932:1308"
-          x={-9.03}
-          y={-5.03}
-          width={165.06}
-          height={57.06}
-          filterUnits="userSpaceOnUse"
-          colorInterpolationFilters="sRGB"
-        >
-          <feBlend
-            in="SourceGraphic"
-            in2="effect1_dropShadow_3932:1308"
-            result="shape"
-          />
-        </filter>
-      </defs>
     </svg>
   );
 }
