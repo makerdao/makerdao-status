05f880eca9c64f6f233a162110f4c4e7ce0a6108
YanierAlf <92033451+YanierAlf@users.noreply.github.com>
2022-02-23 10:15:43 -0300

    fix overview in table (#99)

--- src/components/SummaryOverview/SummaryOverview.tsx
+++ src/components/SummaryOverview/SummaryOverview.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React from 'react';
+import useResizeObserver from '@react-hook/resize-observer';
+import React, { useMemo, useRef } from 'react';
 import { up } from 'styled-breakpoints';
 import styled from 'styled-components';
 import backgroundImg from '../../assets/img/grid.png';
@@ -16,19 +17,40 @@ interface Props {
   }[];
 }
 
+const useSize = (target: any) => {
+  const [size, setSize] = React.useState<{ width: number }>();
+
+  React.useLayoutEffect(() => {
+    setSize(target.current.getBoundingClientRect());
+  }, [target]);
+
+  useResizeObserver(target, (entry) => setSize((entry as any).contentRect));
+  return size;
+};
+
 export default function SummaryOverview({ summaries }: Props) {
+  const ref = useRef();
+  const size = useSize(ref);
+
+  const countCardByRow = useMemo(
+    () => Math.trunc((size?.width || 0) / 241),
+    [size?.width],
+  );
+
   return (
     <Container>
-      <CardsContainer background={backgroundImg}>
+      <CardsContainer background={backgroundImg} ref={ref}>
         {summaries.map((summary, i) => (
           <SummaryCard
             key={Math.random()}
             summary={summary}
-            headerRightBorder={i !== summaries.length - 1}
+            headerRightBorder={
+              i !== summaries.length - 1 && countCardByRow - 1 !== i
+            }
             headerBorderRadius={`${i === 0 ? 10 : 0}px ${
-              i === summaries.length - 1 ? 10 : 0
+              countCardByRow - 1 === i ? 10 : 0
             }px 0px 0px`}
-            notRightBorder={i === summaries.length - 1}
+            notRightBorder={i === countCardByRow - 1}
           />
         ))}
       </CardsContainer>
