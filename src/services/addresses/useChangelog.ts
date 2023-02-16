d16f98d20c2fd0f65ba268d0bef5cc9dbc241b90
Christian Boffill <christian.boffill@gmail.com>
2023-02-16 13:15:26 -0500

    fix: useChangelog hook and use the last URL. Change v1/experimental to v1/governance in useLoadSpells hook

--- src/services/addresses/useChangelog.ts
+++ src/services/addresses/useChangelog.ts
@@ -1,9 +1,23 @@
-import useFetch from '../useFetch';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const URL = 'https://changelog.makerdao.com/releases/mainnet/1.9.12/contracts.json';
 
 const useChangelog = () => {
-  const { data, loading, error } = useFetch(
-    'https://chainlog.makerdao.com/api/mainnet/active.json',
-  );
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | undefined>();
+
+  useEffect(() => {
+    axios.get(URL)
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      .then((res: any) => {
+        setData(res.data);
+        setLoading(false);
+      })
+      .catch(() => setError('Error'));
+  }, []);
+
   return { data, loading, error };
 };
 
